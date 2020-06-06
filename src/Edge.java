faf9b313bbb98ff99d0ffe08b5b0c892f926b0d5
Ferry Ardianto Rismawan <44900042+fardir@users.noreply.github.com>
2020-06-06 08:31:50 +0700

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,17 +1,17 @@
-//public class Edge {
-//    final Node KONEKSI;
-//    final int berat;
-//
-//    public Edge(Node tujuan, int cost) {
-//        this.KONEKSI = tujuan;
-//        this.berat = cost;
-//    }
-//
-//    public Node getKoneksi() {
-//        return KONEKSI;
-//    }
-//
-//    public int getBerat() {
-//        return berat;
-//    }
-//}
+public class Edge {
+   final Node KONEKSI;
+   final int berat;
+
+   public Edge(Node tujuan, int cost) {
+       this.KONEKSI = tujuan;
+       this.berat = cost;
+   }
+
+   public Node getKoneksi() {
+       return KONEKSI;
+   }
+
+   public int getBerat() {
+       return berat;
+   }
+}
