5a89c92bdadb6dcc9dd1cc5b084c7e70a5545444
Ferry Ardianto Rismawan <44900042+fardir@users.noreply.github.com>
2020-06-06 08:31:31 +0700

    Update EdgeA.java

--- src/EdgeA.java
+++ src/EdgeA.java
@@ -1,10 +1,10 @@
-//public class EdgeA {
-//    final double cost;
-//    final NodeA target;
-//
-//    EdgeA(NodeA targetNode, double costVal) {
-//        target = targetNode;
-//        cost = costVal;
-//    }
-//
-//}
+public class EdgeA {
+   final double cost;
+   final NodeA target;
+
+   EdgeA(NodeA targetNode, double costVal) {
+       target = targetNode;
+       cost = costVal;
+   }
+
+}
