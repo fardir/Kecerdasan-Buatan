9ced844326be420360bba9ee315b71c02192cd05
Ferry Ardianto Rismawan <44900042+fardir@users.noreply.github.com>
2020-06-06 08:30:26 +0700

    Update NodeA.java

--- src/NodeA.java
+++ src/NodeA.java
@@ -1,17 +1,17 @@
-//public class NodeA {
-//    final String value;
-//    double gn;
-//    final double hn;
-//    double fn = 0;
-//    EdgeA[] tujuan;
-//    NodeA parent;
-//
-//    NodeA(String val, double hn) {
-//        value = val;
-//        this.hn = hn;
-//    }
-//
-//    public String toString() {
-//        return value;
-//    }
-//}
+public class NodeA {
+   final String value;
+   double gn;
+   final double hn;
+   double fn = 0;
+   EdgeA[] tujuan;
+   NodeA parent;
+
+   NodeA(String val, double hn) {
+       value = val;
+       this.hn = hn;
+   }
+
+   public String toString() {
+       return value;
+   }
+}
